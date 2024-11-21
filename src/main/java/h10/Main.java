315b031365da4c7bd36d22ac6bd422e45023a9db
nyanyan <zentox@users.noreply.github.com>
2024-11-21 10:08:15 +0100

    Remove obsolete code

--- src/main/java/h10/Main.java
+++ src/main/java/h10/Main.java
@@ -1,7 +1,5 @@
 package h10;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         // Manual testing
@@ -9,13 +7,5 @@ public class Main {
         System.out.println(game);
         CardGamePlayer loser = game.determineLoser();
         System.out.println("The loser is: " + loser.getName());
-        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
-        List.of(1,2,3).forEach(list::add);
-        BidirectionalIterator<Integer> iterator = list.cyclicIterator();
-        System.out.println(iterator.next());
-        System.out.println(iterator.next());
-        System.out.println(iterator.next());
-        iterator.remove();
-       System.out.println(iterator.next());
     }
 }
